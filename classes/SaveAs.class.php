951aaeb8ce2f1002d56c89b0385b02205f1d87d2
Fernando Di Masi <wildiney@gmail.com>
2019-11-12 11:47:33 -0300

    Initial Commmit

--- classes/SaveAs.class.php
+++ classes/SaveAs.class.php
@@ -1,11 +1,12 @@
-<?php 
-/** 
+<?php
+/**
  * CLASS SAVEAS
- * Especially made for extract articles from Indracompany.com
- * 
+ * Especially made for extract articles. Customize as you wish
+ *
  * @version 1.0.0
  */
-class SaveAs{
+class SaveAs
+{
     /**
      * Resource's URL
      *
@@ -42,7 +43,8 @@ class SaveAs{
      * @param string $intro
      * @param string $content
      */
-    public function __construct($url, $title, $intro, $content){
+    public function __construct($url, $title, $intro, $content)
+    {
         $this->url = $url;
         $this->title = $title;
         $this->intro = $intro;
@@ -54,29 +56,31 @@ class SaveAs{
      *
      * @return void
      */
-    protected function header(){
+    protected function header()
+    {
         $data = date("Y-m-d");
-        $filename = $data . "-" . str_replace(" ","-",$this->title) . ".doc";
+        $filename = $data . "-" . str_replace(" ", "-", $this->title) . ".doc";
         header("Content-type: application/vnd.ms-word");
-        header("Content-Disposition: attachment;Filename=$filename");    
+        header("Content-Disposition: attachment;Filename=$filename");
     }
-    
+
     /**
      * Set the file's model
      *
      * @return void
      */
-    public function Doc(){
+    public function Doc()
+    {
         $this->header();
         $html = "<html>";
-        $html.= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">";
-        $html.= "<body style='font-family:Arial, sans-serif;'>";
-        $html.= "<p><small><a href='" . $this->url . "'>" . $this->url . "</a></small></p>";
-        $html.= "<h1>" . $this->title . "</h1>";
-        $html.= $this->intro;
-        $html.= $this->content;
-        $html.= "</body>";
-        $html.= "</html>";
+        $html .= "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">";
+        $html .= "<body style='font-family:Arial, sans-serif;'>";
+        $html .= "<p><small><a href='" . $this->url . "'>" . $this->url . "</a></small></p>";
+        $html .= "<h1>" . $this->title . "</h1>";
+        $html .= $this->intro;
+        $html .= $this->content;
+        $html .= "</body>";
+        $html .= "</html>";
 
         return $html;
     }
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <div class="container">
         <h1>SaveAs->Doc</h1>
         <p>Arraste o botão abaixo para os seus favoritos</p>
-        <p><a class="btn btn-primary" href="javascript:(function(){var script=document.createElement('script'); script.setAttribute('id','gettext'); script.setAttribute('type', 'text/javascript'); script.setAttribute('src','http://indra.slicedpixel.com/saveas/gettext.js'); document.getElementsByTagName('head')[0].appendChild(script);})();">SaveAsDoc</a></p>
+        <p><a class="btn btn-primary" href="javascript:(function(){var script=document.createElement('script'); script.setAttribute('id','gettext'); script.setAttribute('type', 'text/javascript'); script.setAttribute('src','https://slicedpixel.com/repo/saveas/gettext.js'); document.getElementsByTagName('head')[0].appendChild(script);})();">SaveAsDoc</a></p>
     </div>
 </body>
 </html>
